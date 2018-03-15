e39b0eb5228c456521f4e45e6123ece80c7a55f9
MaticBernik <mb2551@student.uni-lj.si>
2018-03-15 20:38:18 +0100

    required changes to cached inverse functions...

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,28 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i<-NULL
+    setInverse<-function(y) i<<-solve(y)
+    getInverse<-function() i
+    set<-function(y){
+      x<<-y
+      i<<-NULL
+    }
+    get<-function() x
+    list(setInverse=setInverse,getInverse=getInverse,get=get)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    i<-x$getInverse()
+    if(!is.null(i)){
+      message("Getting cached data")
+      return(i)
+    }else{
+      x$setInverse(x$get())
+      return(x$getInverse())
+    }
 }
